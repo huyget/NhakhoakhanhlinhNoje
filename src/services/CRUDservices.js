18598629d42ee35b3ffa050d35e9f72a2897e290
huy-get <vovanhuy7497@gmail.com>
2023-09-27 15:35:10 +0700

    them user moi

--- src/services/CRUDservices.js
+++ src/services/CRUDservices.js
@@ -1,4 +1,3 @@
-
 import bcrypt from 'bcryptjs';
 import db from '../models/index';
 import { raw } from 'body-parser';
@@ -9,38 +8,38 @@ const salt = bcrypt.genSaltSync(10);
 
 
 //tao user moi
-let createNewUser = async(data) =>{
-    return new Promise(async(resolve, reject)=>{
-        try {
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastname,
-                address: data.address,
-                phoneNumber: data.PhoneNumber,
-                gender:data.gender === '1' ? true : false,
-                roleId: data.RoleId
-            })
-            resolve ('ok create a new user succeed')
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
-//ma hoa password bang (sequelize hash)
-let hashUserPassword = (password) =>{
-    return new Promise(async(resolve, reject) =>{
-        try {
-            let hashPassword =await bcrypt.hashSync(password, salt);
-            resolve(hashPassword)
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
-//lay ra tat ca user
+let createNewUser = async(data) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastname,
+                    address: data.address,
+                    phoneNumber: data.PhoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.RoleId
+                })
+                resolve('ok create a new user succeed')
+            } catch (e) {
+                reject(e)
+            }
+        })
+    }
+    //ma hoa password bang (sequelize hash)
+let hashUserPassword = (password) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let hashPassword = await bcrypt.hashSync(password, salt);
+                resolve(hashPassword)
+            } catch (e) {
+                reject(e);
+            }
+        })
+    }
+    //lay ra tat ca user
 let getAllUser = () => {
     return new Promise(async(resolve, reject) => {
         try {
@@ -55,54 +54,53 @@ let getAllUser = () => {
 }
 
 // lay ra user theo dia chi id
-let getUserInfoById = (userId) =>{
-    return new Promise(async (resolve, reject) => {
-        try {
-            let user =await db.User.findOne({
-                where :{id :userId},
-                raw: true
-            });
-            if (user) {
-                resolve(user)
-            } else {
-                resolve([])
+let getUserInfoById = (userId) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let user = await db.User.findOne({
+                    where: { id: userId },
+                    raw: true
+                });
+                if (user) {
+                    resolve(user)
+                } else {
+                    resolve([])
+                }
+            } catch (e) {
+                reject(e)
+
             }
-        } 
-        catch (e) {
-            reject(e)
-            
-        }
-    })
-}
-// let updateUserData = (data) =>{
-//     return new Promise( async (resolve, reject) => {
-//         try {
-//             let user =await db.User.findOne({
-//                 where :{id :user.id},
-//                 raw: true
-//             });
-//             if(user){
-//                 resolve(user)
-//             }
-//             else{
-//                 resolve()
-//             }
-//         } catch (e) {
-//             resolve(e)
-//         }
-//     })
-// }
+        })
+    }
+    // let updateUserData = (data) =>{
+    //     return new Promise( async (resolve, reject) => {
+    //         try {
+    //             let user =await db.User.findOne({
+    //                 where :{id :user.id},
+    //                 raw: true
+    //             });
+    //             if(user){
+    //                 resolve(user)
+    //             }
+    //             else{
+    //                 resolve()
+    //             }
+    //         } catch (e) {
+    //             resolve(e)
+    //         }
+    //     })
+    // }
 
 
-let updateUserData = (data) =>{
+let updateUserData = (data) => {
     return new Promise(async(resolve, reject) => {
         //lay ra gia tri tronfg databiyte va so sanh voi gia tri nhap vao bang function : findOne 
         try {
-            let user = await db.User.findOne({  // tao the user va lay gia trij databyte va gan cho user
-                where: {id : data.id},
-                raw : false
-            })
-            //gan gia tri cho nhap tu may vao gia tri lay ra tu databiyte
+            let user = await db.User.findOne({ // tao the user va lay gia trij databyte va gan cho user
+                    where: { id: data.id },
+                    raw: false
+                })
+                //gan gia tri cho nhap tu may vao gia tri lay ra tu databiyte
             if (user) {
                 user.lastName = data.lastName;
                 user.firstName = data.firstName;
@@ -123,47 +121,47 @@ let updateUserData = (data) =>{
 }
 
 // su dung function user.destroy de xoa user duoc lay ra
-let deleteUserData = (userId) =>{
-    return new Promise(async(resolve, reject) => {
-        try {
-            let user = await db.User.findOne({
-                where:{ id: userId}
-            });
-            if (user) {
-                await db.User.destroy({where:{ id: userId}});
-                let AllUsers = await db.User.findAll();
-                resolve(AllUsers)
-            } else {
-                resolve()
+let deleteUserData = (userId) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let user = await db.User.findOne({
+                    where: { id: userId }
+                });
+                if (user) {
+                    await db.User.destroy({ where: { id: userId } });
+                    let AllUsers = await db.User.findAll();
+                    resolve(AllUsers)
+                } else {
+                    resolve()
+                }
+                resolve('thanh cong')
+            } catch (e) {
+                console.log(e)
             }
-            resolve('thanh cong')
-        } catch (e) {
-            console.log(e)
-        }
-    })
-}
-// function deleteUserData(id) {
-//     return new Promise(async(resolve, reject) => {
-//       try {
-//         let user = await db.User.findOne({
-//           where: {id: id}
-//         });
-//         if (user) {
-//           await user.destroy();
-//           let AllUsers = await db.User.findAll();
-//           resolve(AllUsers);
-//         } else {
-//           resolve();
-//         }
-//       } catch (e) {
-//         console.log(e);
-//       }
-//     });
-//   }
+        })
+    }
+    // function deleteUserData(id) {
+    //     return new Promise(async(resolve, reject) => {
+    //       try {
+    //         let user = await db.User.findOne({
+    //           where: {id: id}
+    //         });
+    //         if (user) {
+    //           await user.destroy();
+    //           let AllUsers = await db.User.findAll();
+    //           resolve(AllUsers);
+    //         } else {
+    //           resolve();
+    //         }
+    //       } catch (e) {
+    //         console.log(e);
+    //       }
+    //     });
+    //   }
 module.exports = {
-    createNewUser : createNewUser,
-    getAllUser:getAllUser,
-    getUserInfoById : getUserInfoById,
-    updateUserData : updateUserData,
-    deleteUserData : deleteUserData,
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
+    deleteUserData: deleteUserData,
 }
